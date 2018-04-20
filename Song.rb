79d861f6af6f07fadae0a50805f6c9c6c963287c
Colin Tindle <ampersand@underscore.space>
2018-04-20 11:49:00 +0100

    created Song class

---
+++ Song.rb
@@ -0,0 +1,8 @@
+class Song
+  attr_reader(:title, :artist)
+
+  def initialize(title, artist)
+    @title = title
+    @artist = artist
+  end
+end
--- spec/Song_spec.rb
+++ spec/Song_spec.rb
@@ -4,6 +4,11 @@ require_relative('../Song')
 
 class TestSong < MiniTest::Test
   def setup
-    
+    @song1 = Song.new('Girlfriend in a Coma', 'The Smiths')
+  end
+
+  def test_song_has_name_and_artist
+    assert_equal('Girlfriend in a Coma', @song1.title)
+    assert_equal('The Smiths', @song1.artist)
   end
 end
